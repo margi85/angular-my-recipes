d5b41bcfd3bcd6d1e280b523f83ca97c424d6350
Margarita Krusharska <margarita.krusharska@gmail.com>
2020-12-18 23:21:31 +0200

    new styles

--- src/app/recipe/new/new.component.ts
+++ src/app/recipe/new/new.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { IRecipe } from 'src/app/shared/interfaces/recipe';
 import { UserService } from 'src/app/user/user.service';
 import { RecipeService } from '../recipe.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
@@ -11,11 +12,22 @@ import { RecipeService } from '../recipe.service';
 })
 
 export class NewComponent implements OnInit {
+
+  f: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private recipeService: RecipeService,
     private router: Router,
     private userService: UserService
-  ) { }
+  ) { 
+    this.f = this.fb.group({
+      title: [ '', [Validators.required, Validators.minLength(4)] ],
+      ingredients: [ '', [Validators.required] ],
+      preparation: [ '', [Validators.required, Validators.minLength(10)] ],
+      imageURL: [ '', [Validators.required] ],
+    });
+  }
 
 
   ngOnInit(): void {
